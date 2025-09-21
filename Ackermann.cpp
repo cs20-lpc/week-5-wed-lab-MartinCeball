82bfaf00fd4a4e841757953f6db90979ed5366f1
MartinCeball <121062706+MartinCeball@users.noreply.github.com>
2025-09-20 23:08:46 -0700

    Update Ackermann.cpp

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+
 using namespace std;
 
-// Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
- // TODO: Complete the code.
+    if (m == 0) {
+        return n + 1;
+    } 
+    else if (n == 0) {
+        return ackermann(m - 1, 1);
+    } 
+    else {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
 }
 
 int main() {
+    /*
     unsigned int m, n;
     cout << "Enter m and n: ";
     cin >> m >> n;
+    */
 
-    cout << "Ackermann(" << m << ", " << n << ") = " 
+    unsigned int m = 3;
+    unsigned int n = 4;
+
+    cout << "Ackermann(" << m << ", " << n << ") = "
          << ackermann(m, n) << endl;
 
     return 0;
